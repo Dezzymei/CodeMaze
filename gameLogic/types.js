3e35f26545ef57c6ce4b2bba1e16384c5b38e8e3
Robert Desmond <robert.desmond@gmail.com>
2022-01-24 09:23:33 +0100

    Displaying the possible instructions and the list of instructions included

--- gameLogic/types.js
+++ gameLogic/types.js
@@ -1,2 +1,7 @@
 export const WALL = "WALL";
 export const FREE = "FREE";
+
+export const UP = "UP";
+export const DOWN = "DOWN";
+export const LEFT = "LEFT";
+export const RIGHT = "RIGHT";
--- pages/level/[levelId].js
+++ pages/level/[levelId].js
@@ -1,16 +1,59 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { useState } from "react";
 
-import { WALL, FREE } from "../../gameLogic/types";
+import { WALL, FREE, UP, DOWN, LEFT, RIGHT } from "../../gameLogic/types";
+
+const cellWidth = 20;
 
 export default function Level() {
   const router = useRouter();
   const { levelId } = router.query;
 
+  const [instructions, setInstructions] = useState([]);
+
   const levelData = [
     [WALL, FREE],
     [WALL, WALL],
   ];
+
+  const possibleInstructions = [
+    { direction: UP, symbol: "⬆️" },
+    { direction: DOWN, symbol: "⬇️" },
+    { direction: LEFT, symbol: "⬅️" },
+    { direction: RIGHT, symbol: "➡️" },
+  ];
+
+  const displayPossibleInstructions = (possibleInstructions) => {
+    return (
+      <div style={{ fontSize: "xx-large" }}>
+        {possibleInstructions.map((instruction) => {
+          return (
+            <>
+              <span
+                onClick={() => {
+                  setInstructions(instructions.concat(instruction));
+                }}
+              >
+                {instruction.symbol}
+              </span>
+              &nbsp;
+            </>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const displayInstructions = (instructions) => {
+    return (
+      <div style={{ fontSize: "xx-large" }}>
+        {instructions.map((instruction) => {
+          return <>{instruction.symbol}</>;
+        })}
+      </div>
+    );
+  };
 
   const constructLevel = (levelData) => {
     const width = levelData.length;
@@ -27,7 +70,18 @@ export default function Level() {
           return (
             <tr>
               {row.map((cell) => {
-                return <td>{cell}</td>;
+                return (
+                  <td
+                    style={{
+                      border: "1px solid",
+                      background: cell === WALL ? "red" : "white",
+                      width: cellWidth,
+                      height: cellWidth,
+                    }}
+                  >
+                    &nbsp;
+                  </td>
+                );
               })}
             </tr>
           );
@@ -42,7 +96,13 @@ export default function Level() {
         <title>Code Maze | Level {levelId}</title>
       </Head>
       <h1>Level {levelId}</h1>
+      <h2>Instructions</h2>
+
+      {displayInstructions(instructions)}
+      <hr />
       {constructLevel(levelData)}
+      <hr />
+      {displayPossibleInstructions(possibleInstructions)}
     </>
   );
 }
